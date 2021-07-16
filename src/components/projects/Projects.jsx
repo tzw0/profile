54e96656fa66d952f24b83ad0a0c993607bbdf6c
tzw0 <e0310410@u.nus.edu>
2021-07-17 00:01:51 +0800

    slider.js

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,8 +1,50 @@
 import "./projects.scss"
+import React from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination } from 'swiper';
+import 'swiper/swiper.scss';
+
+SwiperCore.use([Navigation, Pagination]);
 
 export default function Projects() {
+    const slides = [];
+
+    for (let i = 1; i < 6; i += 1) {
+        slides.push(
+            <SwiperSlide key={`slide-${i}`} tag="li">
+                <img src={"assets/projects/p" + i + ".png"} alt={`Slide ${i}`} />
+            </SwiperSlide>
+        );
+    }
     return (
         <div className="projects" id="projects">
+            {/* <React.Fragment>
+                <Swiper
+                    id="gallery"
+                    tag="section"
+                    wrapperTag="ul"
+                    navigation
+                    pagination
+                    spaceBetween={0}
+                    slidesPerView={1}
+                    onInit={(swiper) => console.log("asdasd")}
+                    onSlideChange={(swiper) => console.log('slide index changed to : ', swiper.activeIndex)}
+                >
+                    {slides}
+                </Swiper>
+            </React.Fragment> */}
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={3}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                ...
+            </Swiper>
         </div>
     )
 }
