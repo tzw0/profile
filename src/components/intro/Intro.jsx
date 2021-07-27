710d433c4f3d9c9497e95c6d632a108809b080a7
tzw0 <e0310410@u.nus.edu>
2021-07-27 17:40:02 +0800

    mobile calibration

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,5 @@
 import './intro.scss'
 import resume from './resume.pdf'
-import {
-    BrowserView,
-    MobileView,
-} from "react-device-detect";
-import { IsLandscape } from '../../utils/window';
 import { ifMobile, ResponsiveButton, ResponsiveHeader } from '../../utils/mobile';
 
 export default function Intro() {
@@ -21,9 +16,6 @@ export default function Intro() {
             <div className="introParagraph">
                 <ResponsiveHeader title="Hi, I'm Zheng Wen" />
                 <p>I am a final year Computer Engineering Student in NUS specialising in Large Scale Computing. I take pride in my work and actively seek for opportunities to expose myself to new and useful tech tools</p>
-                <MobileView><p>Mobile</p></MobileView>
-                <BrowserView><p>Browser</p></BrowserView>
-                <p>{IsLandscape() ? <span>isLandscape</span> : <span>isPotrait</span>}</p>
                 <span className="buttonContainer">
                     <a href={resume} target="_blank" rel="noreferrer"><ResponsiveButton color="primary" title="View Resume" /></a>
                     <a href="#projects"><ResponsiveButton color="secondary" title="view portfolio" /></a>
