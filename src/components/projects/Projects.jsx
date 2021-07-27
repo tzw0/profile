0ea9302c74197d42b1e0f41ee1a2c8f7d4d407e7
tzw0 <e0310410@u.nus.edu>
2021-07-28 01:19:35 +0800

    redeploy

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -74,18 +74,13 @@ const useStyles = makeStyles((theme) => ({
 
     root: {
         "& .MuiInputLabel-outlined:not(.MuiInputLabel-shrink)": {
-            // Default transform is "translate(14px, 20px) scale(1)""
-            // This lines up the label with the initial cursor position in the input
-            // after changing its padding-left.
             transform: "translate(34px, 20px) scale(1);"
         }
     },
     inputRoot: {
         color: "white",
         fontSize: "inherit",
-        // This matches the specificity of the default styles at https://github.com/mui-org/material-ui/blob/v4.11.3/packages/material-ui-lab/src/Autocomplete/Autocomplete.js#L90
         '&[class*="MuiOutlinedInput-root"] .MuiAutocomplete-input:first-child': {
-            // Default left padding is 6px
             paddingLeft: 26
         },
         "& .MuiOutlinedInput-notchedOutline": {
